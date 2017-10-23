a227e9eaf0fa5de8c55b56ac820990bd62c53972
KovaCode <33041161+KovaCode@users.noreply.github.com>
2017-10-23 23:43:11 +0200

    Add files via upload

---
+++ ApplicationEvents.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Data.SqlClient
+
+Namespace My
+
+    ' The following events are available for MyApplication:
+    ' 
+    ' Startup: Raised when the application starts, before the startup form is created.
+    ' Shutdown: Raised after all application forms are closed.  This event is not raised if the application terminates abnormally.
+    ' UnhandledException: Raised if the application encounters an unhandled exception.
+    ' StartupNextInstance: Raised when launching a single-instance application and the application is already active. 
+    ' NetworkAvailabilityChanged: Raised when the network connection is connected or disconnected.
+
+
+
+    Partial Friend Class MyApplication
+
+        Private Sub MyApplication_NetworkAvailabilityChanged(sender As Object, e As Microsoft.VisualBasic.Devices.NetworkAvailableEventArgs) Handles Me.NetworkAvailabilityChanged
+
+        End Sub
+
+        Private Sub MyApplication_Startup(sender As Object, e As Microsoft.VisualBasic.ApplicationServices.StartupEventArgs) Handles Me.Startup
+
+            'CreateWaitDialog(rm.GetString("waitCaption0"), rm.GetString("waitTitle"))
+        End Sub
+
+    End Class
+
+
+End Namespace
+
